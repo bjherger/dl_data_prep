bbb20f8a549022481c239a80dd86a413085a8a02
Brendan J. Herger <b@revup.com>
2017-12-11 16:26:32 -0800

    First pass at full model

--- bin/main.py
+++ bin/main.py
@@ -7,7 +7,11 @@ Code Template
 """
 import logging
 
+import pandas
+from keras import Model, optimizers
+
 import lib
+import df_prep
 
 
 def main():
@@ -18,31 +22,56 @@ def main():
     """
     logging.basicConfig(level=logging.DEBUG)
 
-    extract()
-    transform()
-    model()
+    observations = extract()
+    observations, mapper = transform(observations)
+    model(observations, mapper)
     load()
     pass
 
 
 def extract():
     logging.info('Begin extract')
-
+    reservations = pandas.read_csv('../data/input/air_reserve.csv')
+    visits = pandas.read_csv('../data/input/air_visit_data.csv')
+    observations = pandas.merge(reservations, visits)
     lib.archive_dataset_schemas('extract', locals(), globals())
     logging.info('End extract')
-    pass
+    return observations
 
 
-def transform():
+def transform(observations):
     logging.info('Begin transform')
+
+    cat_vars = ['air_store_id']
+    cont_vars = ['reserve_visitors', 'visitors']
+    date_vars = ['visit_datetime', 'reserve_datetime', 'visit_date']
+    mapper = df_prep.create_mapper(observations, cat_vars=cat_vars, cont_vars=cont_vars)
 
     lib.archive_dataset_schemas('transform', locals(), globals())
     logging.info('End transform')
-    pass
+    return observations, mapper
 
 
-def model():
+def model(observations, mapper):
     logging.info('Begin model')
+
+    cat_vars = ['air_store_id']
+    cont_vars = ['reserve_visitors']
+    date_vars = ['visit_datetime', 'reserve_datetime', 'visit_date']
+    response_var = 'visitors'
+
+    Xs, y, x_inputs, input_nub, output_nub = df_prep.create_model_layers(observations, cat_vars, cont_vars, response_var, mapper)
+
+    # Create model
+    x = input_nub
+    preds = output_nub(x)
+
+    regression_model = Model(x_inputs, preds)
+    opt = optimizers.Adam()
+    regression_model.compile(loss='mean_squared_error',
+                             optimizer=opt)
+
+    regression_model.fit(Xs, y)
 
     lib.archive_dataset_schemas('model', locals(), globals())
     logging.info('End model')
